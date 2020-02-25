e6238def64f446b16ea4c371a648983e1ec9fdc0
marcetin <marcetin@gmail.com>
2020-02-25 19:18:57 +0100

    Fix TextSize Field

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"gioui.org/app"
+	"gioui.org/f32"
 	"gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
+	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"github.com/gop9/gorminal/mod"
+	"image"
+	"image/color"
 	"os/exec"
 	"strings"
 	"time"
@@ -25,6 +29,8 @@ var (
 		Axis:        layout.Vertical,
 		ScrollToEnd: true,
 	}
+	fontSize  float32 = 16
+	textColor         = color.RGBA{A: 0xff, R: 0x30, G: 0xcf, B: 0x30}
 )
 
 func main() {
@@ -39,7 +45,7 @@ func main() {
 		for e := range w.Events() {
 			if e, ok := e.(system.FrameEvent); ok {
 				gtx.Reset(e.Config, e.Size)
-
+				fill(gtx)
 				layout.Flex{}.Layout(gtx,
 					layout.Flexed(1, func() {
 						layout.UniformInset(unit.Dp(0)).Layout(gtx, func() {
@@ -54,35 +60,44 @@ func main() {
 											Alignment: layout.End,
 										}.Layout(gtx,
 											layout.Rigid(func() {
-												sat := th.Body1("ds://" + t.ComID + "_" + t.Out)
-												sat.Font.Size = unit.Dp(16)
-												sat.Layout(gtx)
+												out := th.Body1(t.Out)
+												out.TextSize = unit.Dp(fontSize)
+												out.Color = textColor
+												out.Layout(gtx)
 											}),
 										)
 									})
 								}),
 								layout.Rigid(func() {
-									layout.UniformInset(unit.Dp(8)).Layout(gtx, func() {
-										e := th.Editor("Run command")
-										e.Font.Style = text.Regular
-										e.Font.Size = unit.Dp(16)
-										e.Layout(gtx, consoleInputField)
-										for _, e := range consoleInputField.Events(gtx) {
-											if e, ok := e.(widget.SubmitEvent); ok {
-
-												splitted := strings.Split(e.Text, " ")
-
-												cmd, _ := exec.Command(splitted[0], splitted[1:]...).Output()
-
-												com.Commands = append(com.Commands, mod.Command{
-													ComID: e.Text,
-													Time:  time.Time{},
-													Out:   string(cmd),
-												})
-												consoleInputField.SetText("")
-											}
+									pwd, _ := exec.Command("pwd").Output()
+									layout.Flex{}.Layout(gtx,
+										layout.Rigid(func() {
+											p := th.Body1(out(pwd))
+											p.Font.Style = text.Regular
+											p.TextSize = unit.Dp(fontSize)
+											p.Color = textColor
+											p.Layout(gtx)
+										}),
+										layout.Rigid(func() {
+											input := th.Editor("")
+											input.Font.Style = text.Regular
+											input.TextSize = unit.Dp(fontSize)
+											input.Color = textColor
+											input.Layout(gtx, consoleInputField)
+										}),
+									)
+									for _, e := range consoleInputField.Events(gtx) {
+										if e, ok := e.(widget.SubmitEvent); ok {
+											splitted := strings.Split(e.Text, " ")
+											cmd, _ := exec.Command(splitted[0], splitted[1:]...).CombinedOutput()
+											com.Commands = append(com.Commands, mod.Command{
+												ComID: e.Text,
+												Time:  time.Time{},
+												Out:   out(cmd),
+											})
+											consoleInputField.SetText("")
 										}
-									})
+									}
 								}))
 						})
 					}),
@@ -92,4 +107,19 @@ func main() {
 		}
 	}()
 	app.Main()
+}
+
+func out(i []byte) string {
+	return "[" + string(i) + "]$"
+}
+
+func fill(gtx *layout.Context) {
+	cs := gtx.Constraints
+	d := image.Point{X: cs.Width.Min, Y: cs.Height.Min}
+	dr := f32.Rectangle{
+		Max: f32.Point{X: float32(d.X), Y: float32(d.Y)},
+	}
+	paint.ColorOp{Color: color.RGBA{A: 0xff, R: 0x30, G: 0x30, B: 0x30}}.Add(gtx.Ops)
+	paint.PaintOp{Rect: dr}.Add(gtx.Ops)
+	gtx.Dimensions = layout.Dimensions{Size: d}
 }
